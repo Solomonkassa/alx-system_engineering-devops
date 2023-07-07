38657a5fe1ef010e1168282714438097be399446
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-07-07 15:46:18 +0300

    Update 0-world_wide_web.sh

--- 0x10-https_ssl/0-world_wide_web.sh
+++ 0x10-https_ssl/0-world_wide_web.sh
@@ -1,42 +1,19 @@
 #!/usr/bin/env bash
+# Usage: ./1-world_wide_web <domain> <subdomain>
 # Display information about subdomains.
 
 domain_information () {
-    subdomain=$1
-    ip=$2
-    state=$3
-
-    echo "The subdomain $subdomain is a A record and points to $ip (State: $state)"
+    line=$(dig "$2"."$1" | grep -A1 'ANSWER SECTION:' | tr '\t' '\n' | tail -2 | tr '\n' ' ')
+    echo "$2 $line" | awk '{print "The subdomain " $1 " is a " $2 " record and points to " $3}'
 }
-
 
 if [ "$#" == 1 ]
 then
-    domain=$1
-    domain_information "www" "54.210.47.110" "running"
-    domain_information "lb-01" "35.153.16.72" "running"
-    domain_information "web-01" "54.237.117.138" "running"
-    domain_information "web-02" "100.26.216.97" "running"
+  domain_information "$1" "www"
+  domain_information "$1" "lb-01"
+  domain_information "$1" "web-01"
+  domain_information "$1" "web-02"
 elif [ "$#" == 2 ]
 then
-    domain=$1
-    subdomain=$2
-
-    case $subdomain in
-        "www")
-            domain_information "www" "54.210.47.110" "running"
-            ;;
-        "lb-01")
-            domain_information "lb-01" "35.153.16.72" "running"
-            ;;
-        "web-01")
-            domain_information "web-01" "54.237.117.138" "running"
-            ;;
-        "web-02")
-            domain_information "web-02" "100.26.216.97" "running"
-            ;;
-        *)
-            echo "Invalid subdomain"
-            ;;
-    esac
+  domain_information "$1" "$2"
 fi
